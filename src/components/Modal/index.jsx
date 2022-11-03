ecdd09fcb1e7bcc0c7a69b5b4ea4a70ea95cf289
Giuliano Cappa <cappa.giuliano@gmail.com>
2022-11-03 19:58:56 -0300

    Crear Modal y ModalVictory

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,26 @@
+function Modal({ children, styles, setClose }) {
+  return (
+    <>
+      <div
+        className="absolute h-full w-full bg-black opacity-60 z-10"
+        id="back-modal"
+        onClick={() => setClose((prevValue) => !prevValue)}
+      ></div>
+      <article
+        className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 ${styles}`}
+      >
+        <button
+          className="absolute top-[6%] right-[13%] cursor-default"
+          onClick={() => setClose((prevValue) => !prevValue)}
+        >
+          <figure>
+            <img src="/close.png" alt="" />
+          </figure>
+        </button>
+        {children}
+      </article>
+    </>
+  )
+}
+
+export default Modal
