45c40cafea4195d2dec97ec487b88904ecd5b31f
Giuliano Cappa <cappa.giuliano@gmail.com>
2022-11-04 17:14:46 -0300

    Crear componente TimeTrial

--- src/components/TimeTrial/index.jsx
+++ src/components/TimeTrial/index.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react'
+
 import millisecondsToMinutesSeconds from '../../utils/millisecondsToMinutesSeconds'
 
-const total_time = 5 * 60 * 1000
+import { total_time } from '../../consts/game'
 
 function TimeTrial({ initTime, status, onFinish }) {
   const [time, setTime] = useState(total_time)
@@ -9,7 +10,10 @@ function TimeTrial({ initTime, status, onFinish }) {
   useEffect(() => {
     let left_time = null
 
-    if (time === 0) {
+    if (time <= 0) {
+      clearInterval(left_time)
+
+      setTime(total_time)
       onFinish((stateGame) => {
         return { ...stateGame, status: 'defeat' }
       })
@@ -28,9 +32,10 @@ function TimeTrial({ initTime, status, onFinish }) {
     return () => {
       if (left_time) clearInterval(left_time)
     }
-  }, [initTime, status])
+  }, [initTime, status, time])
 
   let [minutes, seconds] = millisecondsToMinutesSeconds(time)
+
   return (
     <div className="mb-4 text-4xl text-center text-[#cd0300]">
       Tiempo restante {minutes}:{seconds}
